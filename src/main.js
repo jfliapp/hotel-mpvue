67bd2f766c4b402b9bc997fa11bfdb58e5ca5d3c
jianfengli <13527817714@163.com>
2018-07-09 18:41:25 +0800

    add filter-item

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ export default {
   // 这个字段走 app.json
   config: {
     // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
-    pages: ['pages/mains/main', '^pages/orderDetail/main'],
+    pages: ['^pages/mains/main', 'pages/orderDetail/main'],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#004891',
@@ -22,19 +22,27 @@ export default {
       list: [
         {
           'pagePath': 'pages/mains/main',
-          'text': 'main'
+          'iconPath': '/static/imgs/tablist_home.png',
+          'selectedIconPath': '/static/imgs/tablist_home.png',
+          // 'text': 'main'
         },
         {
           'pagePath': 'pages/map/main',
-          'text': 'Map'
+          'iconPath': '/static/imgs/tablist_map.png',
+          'selectedIconPath': '/static/imgs/tablist_map.png',
+          // 'text': 'Map'
         },
         {
           'pagePath': 'pages/collect/main',
-          'text': 'Collect'
+          'iconPath': '/static/imgs/tablist_shop.png',
+          'selectedIconPath': '/static/imgs/tablist_shop.png',
+          // 'text': 'Collect'
         },
         {
           'pagePath': 'pages/myorder/main',
-          'text': 'MyOrder'
+          'iconPath': '/static/imgs/tablist_myorder.png',
+          'selectedIconPath': '/static/imgs/tablist_myorder.png',
+          // 'text': 'MyOrder'
         }
       ]
     }
--- src/pages/mains/index.vue
+++ src/pages/mains/index.vue
@@ -1,5 +1,103 @@
 <template>
-  <div>
+  <div style="width: 100%;height: 100%;">
+    <!-- 这里开始弹框 -->
+    <div class="mask" v-show="tapState">
+      <div style="display: flex; flex-direction: column;">
+        <div style="display:flex;align-items:center;justify-content:center;height:50px;background: rgb(0, 126, 226)">上海<img src="/static/imgs/down.png" style="width: 15px;height: 15px;"></div>
+        <div>
+          <div class="filter_item">
+              <div @click="filterItem(item)" v-for="(item, index) in filter_item" :key="index">
+                <div>
+                  <span :class="{click_change: item.active}">{{item.name}}&nbsp;&nbsp;</span>
+                  <img v-if="!item.active" class="img_class" src="/static/imgs/down_san1.png"/>
+                  <img v-else class="img_class" src="/static/imgs/up_san.png"/> |
+                </div>
+              </div>
+              <div style="color: #1296db" @click="likeFli">&nbsp;欢迎度排序<img class="img_class" src="../../../static/imgs/down_san.png"/></div>
+            </div>
+        </div>
+        <div v-if="tapState == 'filter'" class="F_model">
+            <div class="F_model_T">
+              <div class="F_model_T_L">
+                <div v-for="(item, iFilter) in arr1" :key="iFilter"
+                class="F_model_tl"
+                :class="{F_model_tlF: item.border}"
+                @click="clickL(item)">{{item.name}}</div>
+              </div>
+              <div class="F_model_T_R">
+                <div v-for="(arrI, idx) in arrItemL" :key="idx">
+                  <div style="padding: 5px;">{{arrI.name}}</div>
+                  <div style="display: flex;flex-wrap: wrap;">
+                    <div v-for="(item, index) in (arrI.tab ? arrI.arr : arrI.arrSlice)"
+                     :key="index"
+                     class="F_model_T_R_item">{{item}}</div>
+                    <div v-if="arrI.tab" @click="arrI.tab = !arrI.tab" class="F_model_T_R_item_more">收缩<img src="/static/imgs/up_san.png" style="width: 10px;height: 10px;margin-left: 10px;"></div>
+                    <div v-else class="F_model_T_R_item_more" @click="arrI.tab = !arrI.tab">更多<img src="/static/imgs/down.png" style="width: 10px;height: 10px;margin-left: 10px;"></div>
+                    <!-- <div v-if="arrI.arr.length === moreLen" style="width: 60px;height: 30px;background: yellow;text-align: center;line-height: 30px;margin:5px;" @click="ItemDiv(arrI.arr)">收缩</div> -->
+                  </div>
+                </div>  
+              </div>
+            </div>
+            <div class="F_model_B" style="display: flex;">
+              <div style="width: 50%;text-align: center;height: 43px;line-height: 43px">清空</div>
+              <div style="width: 50%;text-align: center;height: 43px;line-height: 43px;background: rgb(254, 105, 19);"
+              @click="sureF">确定</div>
+            </div>
+        </div>
+        <div v-else-if="tapState == 'map'" class="F_model">
+          <div class="F_model_T">
+            <div style="display: flex;flex-direction: column;flex: 1.5;background: rgb(245, 245, 245);overflow-y: scroll">
+              <div v-for="(item, iFilter1) in arr2" :key="iFilter1"
+              class="F_model_tl"
+              :class="{F_model_tlF: item.border}"
+              @click="clickL2(item)">{{item.name}}</div>
+            </div>
+            <div style="display: flex;flex-direction: column;flex: 4;overflow-y: scroll">
+              <div v-for="(arrI, idxDist) in arrDistance" :key="idxDist"
+              style="display: flex;justify-content: space-between;padding: 5px 10px;border-bottom: 1px solid rgb(235, 235, 235);font-size: 15px"
+              :class="{dist_color: arrI.checked}"
+              @click="check(arrI)">
+                <div>{{arrI.value}}</div>
+                <div :hidden="!arrI.checked">✔</div>
+              </div>  
+            </div>
+          </div>
+          <div class="F_model_B" style="display: flex;">
+            <div style="width: 50%;text-align: center;height: 43px;line-height: 43px">清空</div>
+            <div style="width: 50%;text-align: center;height: 43px;line-height: 43px;background: rgb(254, 105, 19);"
+            @click="sureF">确定</div>
+          </div>
+        </div>
+        <div v-else-if="tapState == 'price'" class="F_model">
+          <div class="F_model_T_price">
+            <div style="display: flex;flex-direction: column;margin-top: 15px;">
+              <div style="margin-bottom: 15px;">星级(可复选)</div> 
+              <div style="display: flex;justify-content: space-around">
+                <div v-for="(item, ind) in star" :key="ind" class="star" :class="{star_choose: item.state}" @click=starChoose(item)>
+                  {{item.name}}
+                </div>
+              </div>
+            </div>
+            <div style="display: flex;flex-direction: column;margin-top: 15px;">
+              <div style="margin-bottom: 15px;">价格</div> 
+              <div style="display: flex;flex-direction: column">
+                <div style="display: flex;justify-content: space-between">
+                  <div>￥0</div>
+                  <div>￥10000</div>
+                </div>
+                <slider step="1" backgroundColor="rgb(0, 126, 226)" height="3px"></slider>
+              </div>
+            </div>
+          </div>
+          <div class="F_model_B" style="display: flex;">
+            <div style="width: 50%;text-align: center;height: 43px;line-height: 43px">清空</div>
+            <div style="width: 50%;text-align: center;height: 43px;line-height: 43px;background: rgb(254, 105, 19);"
+            @click="sureF">确定</div>
+          </div>
+        </div>
+        <div v-else></div>
+      </div>      
+    </div>
     <div class="sub_title">
       <div style="width: 100%;height: 20px;display: flex;justify-content: flex-end">
         <div style="display: flex;align-items: center;margin-right: 30%">上海<img src="../../../static/imgs/down.png" alt="" style="width: 15px;height: 15px;margin-left: 5px"></div>
@@ -17,7 +115,7 @@
             <!-- <div style="float: left;"><img src="../../../static/imgs/filter_line.png" alt="" style="width: 15px;height: 40px;"></div> -->
           </div>
           <div>
-            <div style="float: left;margin-left: 5px;margin-top: 9px;"><img src="../../../static/imgs/search.png" alt="" class="img_class"></div>
+            <div style="float: left;margin-left: 5px;margin-top: 9px;"><img src="../../../static/imgs/search.png" style="width: 15px;height: 15px;"></div>
             <div><input style="height: 40px;padding-left: 3px" type="text" placeholder="关键字/位置/品牌/酒店名"></div>
           </div>          
         </div>
@@ -32,7 +130,7 @@
           <img v-else class="img_class" src="../../../static/imgs/up_san.png"/> |
         </div>
       </div>
-      <div style="color: #1296db" @click="likeFli">&nbsp;欢迎度排序<img class="img_class" src="../../../static/imgs/down_san.png"/></div>
+      <div style="color: #1296db">&nbsp;欢迎度排序<img class="img_class" src="../../../static/imgs/down_san.png"/></div>
     </div>
     <div class="filter_item" style="background: rgb(244, 244, 246);margin-top: 8px;padding: 10px 0">
       <div class="filter_li">低价好评</div>
@@ -63,97 +161,17 @@
           </div>
         </div>
       </div>
-    </div>
-    <!-- 这里开始弹框 -->
-    <!-- <div style="height: 350px;background: yellowgreen;"> -->
-      <div v-if="tapState == 'filter'" class="F_model">
-        <div class="F_model_T" style="display: flex;height: 380px;">
-          <div style="display: flex;flex-direction: column;flex: 1.5;background: rgb(245, 245, 245);overflow-y: scroll">
-            <div v-for="(item, iFilter) in arr1" :key="iFilter"
-            class="F_model_tl"
-            :class="{F_model_tlF: item.border}"
-            @click="clickL(item)">{{item.name}}</div>
-          </div>
-          <div style="display: flex;flex-direction: column;flex: 4;overflow-y: scroll">
-            <div v-for="(arrI, idx) in arrItemL" :key="idx">
-              <div style="font-size: 15px;padding: 5px;">{{arrI.name}}</div>
-              <div style="display: flex;flex-wrap: wrap;font-size: 15px;">
-                <div v-for="(item, index) in (arrI.tab ? arrI.arr : arrI.arrSlice)" :key="index"style="width: 60px;height: 30px;background:rgb(237, 245, 251);text-align: center;line-height: 30px;margin:5px;">{{item}}</div>
-                <div v-if="arrI.tab" style="width: 60px;height: 30px;background: yellow;text-align: center;line-height: 30px;margin:5px;" @click="arrI.tab = !arrI.tab">收缩</div>
-                <div v-else style="width: 60px;height: 30px;background: yellow;text-align: center;line-height: 30px;margin:5px;" @click="arrI.tab = !arrI.tab">更多</div>
-                <!-- <div v-if="arrI.arr.length === moreLen" style="width: 60px;height: 30px;background: yellow;text-align: center;line-height: 30px;margin:5px;" @click="ItemDiv(arrI.arr)">收缩</div> -->
-              </div>
-            </div>  
-          </div>
-        </div>
-        <div class="F_model_B" style="display: flex;">
-          <div style="width: 50%;text-align: center;height: 43px;line-height: 43px">清空</div>
-          <div style="width: 50%;text-align: center;height: 43px;line-height: 43px;background: rgb(254, 105, 19);"
-          @click="sureF">确定</div>
-        </div>
-      </div>
-      <div v-else-if="tapState == 'map'" class="F_model">
-        <div class="F_model_T" style="display: flex;height: 380px;">
-          <div style="display: flex;flex-direction: column;flex: 1.5;background: rgb(245, 245, 245);overflow-y: scroll">
-            <div v-for="(item, iFilter1) in arr2" :key="iFilter1"
-            class="F_model_tl"
-            :class="{F_model_tlF: item.border}"
-            @click="clickL2(item)">{{item.name}}</div>
-          </div>
-          <div style="display: flex;flex-direction: column;flex: 4;overflow-y: scroll">
-            <div v-for="(arrI, idxDist) in arrDistance" :key="idxDist"
-            style="display: flex;justify-content: space-between;padding: 5px 10px;border-bottom: 1px solid rgb(235, 235, 235);font-size: 15px"
-            :class="{dist_color: arrI.checked}"
-            @click="check(arrI)">
-              <div>{{arrI.value}}</div>
-              <div :hidden="!arrI.checked">✔</div>
-            </div>  
-          </div>
-        </div>
-        <div class="F_model_B" style="display: flex;">
-          <div style="width: 50%;text-align: center;height: 43px;line-height: 43px">清空</div>
-          <div style="width: 50%;text-align: center;height: 43px;line-height: 43px;background: rgb(254, 105, 19);"
-          @click="sureF">确定</div>
-        </div>
-      </div>
-      <div v-else-if="tapState == 'price'" class="F_model">
-        <div class="F_model_T" style="display: flex;height: 380px;flex-direction: column;padding:0 15px;">
-          <div style="display: flex;flex-direction: column;margin-top: 15px;">
-            <div style="font-size: 17px;margin-bottom: 15px;">星级(可复选)</div> 
-            <div style="display: flex;justify-content: space-around">
-              <div v-for="(item, ind) in star" :key="ind" class="star" :class="{star_choose: item.state}" @click=starChoose(item)>
-                {{item.name}}
-              </div>
-            </div>
-          </div>
-          <div style="display: flex;flex-direction: column;margin-top: 15px;">
-            <div style="font-size: 17px;margin-bottom: 15px;">价格</div> 
-            <div style="display: flex;flex-direction: column">
-              <div style="display: flex;justify-content: space-between">
-                <div>￥0</div>
-                <div>￥10000</div>
-              </div>
-              <slider step="1" backgroundColor="rgb(0, 126, 226)" height="3px"></slider>
-            </div>
-          </div>
-        </div>
-        <div class="F_model_B" style="display: flex;">
-          <div style="width: 50%;text-align: center;height: 43px;line-height: 43px">清空</div>
-          <div style="width: 50%;text-align: center;height: 43px;line-height: 43px;background: rgb(254, 105, 19);"
-          @click="sureF">确定</div>
-        </div>
-      </div>
-      <div v-else></div>
-    <!-- </div> -->
+    </div>    
   </div>
 </template>
 <script>
   import img from '../../../static/imgs/hotel_item.png'
-
+  // import sort from '@/components/sort'
   export default {
     name: 'main',
     data () {
       return {
+        F_item: false,
         color_change: true,
         moreLen: 0,
         star: [
@@ -179,7 +197,7 @@
             id: 4,
             value: 5,
             state: false,
-            name: '五星以上/舒适'
+            name: '五星/舒适'
           }
         ],
         arrDistance: [
@@ -247,21 +265,21 @@
             name: '高端连锁',
             idx: 0,
             tab: false,
-            arrSlice: ['xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx'],
+            arrSlice: ['xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx'],
             arr: ['xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx']
           },
           {
             name: '中端连锁',
             idx: 1,
             tab: true,
-            arrSlice: ['xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx'],
+            arrSlice: ['xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx'],
             arr: ['xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx']
           },
           {
             name: '快捷连锁',
             idx: 2,
             tab: false,
-            arrSlice: ['xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx'],
+            arrSlice: ['xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx'],
             arr: ['xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx', 'xx']
           }
         ],
@@ -313,13 +331,14 @@
         ]
       }
     },
+    components: {},
     computed: {
       tapState () {
         let sta = this.filter_item.find((item) => {
           return item.active
         })
         if (sta === undefined) {
-          return 'xx'
+          return false
         }
         return sta.state
       }
@@ -374,6 +393,10 @@
           }
         })
       },
+      likeFli () {
+        console.log('xxx')
+        this.sureF()
+      },
       check (item) {
         console.log(item)
         let distId = item.id
@@ -410,6 +433,14 @@
     height: 0;
     color: transparent;   
   }
+  .mask {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    overflow: hidden;
+    z-index: 99999999999999;
+    background: rgba(15, 15, 26, 0.7)
+  }
   .sub_title{ 
     width: 100%;
     height: 100px;
@@ -437,6 +468,8 @@
     font-size: 12px;
   } */
   .filter_item {
+    border-bottom: 1px solid #ccc;
+    padding-bottom: 2px;
     display: flex; 
     justify-content: space-around;
     font-size: 14px;
@@ -527,18 +560,65 @@
   .F_model {
     height: 400px;
     display: flex;
+    font-size: 15px;
     flex-direction: column;
-    position: fixed;
-    top: 22%;width: 100%;
+    /* position: fixed;
+    top: 22%;
+    width: 100%; */
     background: white
+  }
+  .F_model_T {
+    display: flex;
+    height: 380px;
+  }
+  .F_model_T_L {
+    display: flex;
+    flex-direction: column;
+    flex: 1.5;
+    background: rgb(245, 245, 245);
+    overflow-y: scroll
   }
   .F_model_tl {
     height: 40px;
     line-height: 40px;
     text-align: center;
-    font-size: 17px;
     border-bottom: 2px solid rgb(236, 236, 236);
   }
+.F_model_T_R {
+  display: flex;
+  flex-direction: column;
+  font-size: 13px;
+  flex: 4;
+  margin-left: 10px;
+  overflow-y: scroll
+}
+.F_model_T_R_item {
+  width: 80px;
+  height: 30px;
+  background:rgb(237, 245, 251);
+  text-align: center;
+  line-height: 30px;
+  margin:2px;
+}
+.F_model_T_R_item_more {
+  width: 80px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  background: yellow;
+  justify-content: center;
+  line-height: 30px;
+  margin:5px;
+}
+.F_model_T_price {
+  display: flex;
+  height: 380px;
+  flex-direction: column;
+  padding:0 15px;
+}
+.F_model_B {
+  font-size: 15px;
+}
   .F_model_tlF {
     border-left: 3px solid rgb(0, 72, 145);
     background: white    
@@ -548,10 +628,12 @@
   }
   .star {
     height: 43px;
-    width: 82px;
+    width: 80px;
     background:rgb(237, 245, 251);
-    /* text-align: center; */
-    padding: 3px auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* padding: 3px auto; */
     font-size: 13px;
   }
   .star_choose {
